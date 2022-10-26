f3dc7a83a16f74a29b818a3b7a796ff0a2a5761d
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-10-26 09:25:32 +0700

    Setup-setup-code-base-entity,oe1

---
+++ src/main/java/cybersoft/javabackend/java18/obajuecommerce/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package cybersoft.javabackend.java18.obajuecommerce.common.exception;
+
+import cybersoft.javabackend.java18.obajuecommerce.common.model.ResponseDTO;
+import cybersoft.javabackend.java18.obajuecommerce.common.utils.ResponseUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ResponseDTO> handlerMaxUploadSizeExceededException() {
+        return ResponseUtils.error(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ResponseDTO> handlerConstraintViolationException(ConstraintViolationException exception) {
+        return ResponseUtils.error(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ResponseDTO> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseUtils.error(exception, HttpStatus.BAD_REQUEST);
+    }
+}
