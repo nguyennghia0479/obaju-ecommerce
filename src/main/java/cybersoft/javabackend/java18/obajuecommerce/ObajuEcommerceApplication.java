e99b52a45dc17bdfcd7872c442d908b08b2a526f
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-10-26 08:18:57 +0700

    Init project

---
+++ src/main/java/cybersoft/javabackend/java18/obajuecommerce/ObajuEcommerceApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java18.obajuecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObajuEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObajuEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/javabackend/java18/obajuecommerce/ObajuEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java18.obajuecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObajuEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
