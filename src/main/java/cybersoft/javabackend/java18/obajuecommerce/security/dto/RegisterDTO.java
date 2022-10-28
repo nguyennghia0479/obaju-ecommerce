8213d6a0468617b0665f6ea7f45ccdae6cac06dc
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-10-28 14:41:36 +0700

    feat: create-user-register, OE-9

---
+++ src/main/java/cybersoft/javabackend/java18/obajuecommerce/security/dto/RegisterDTO.java
@@ -0,0 +1,42 @@
+package cybersoft.javabackend.java18.obajuecommerce.security.dto;
+
+import cybersoft.javabackend.java18.obajuecommerce.user.validation.annotation.UniqueEmail;
+import cybersoft.javabackend.java18.obajuecommerce.user.validation.annotation.UniquePhoneNum;
+import cybersoft.javabackend.java18.obajuecommerce.user.validation.annotation.UniqueUsername;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterDTO implements Serializable {
+    @Size(min = 5, max = 100, message = "{user.username.size}")
+    @NotBlank(message = "{user.username.blank}")
+    @UniqueUsername
+    private String username;
+
+    @Size(min = 5, max = 100, message = "{user.password.size}")
+    @NotBlank(message = "{user.password.blank}")
+    private String password;
+
+    @Size(min = 5, max = 100, message = "{user.email.size}")
+    @NotBlank(message = "{user.email.blank}")
+    @UniqueEmail
+    @Email(message = "{user.email.valid}")
+    private String email;
+
+    @Size(min = 5, max = 100, message = "{user.fullName.size}")
+    @NotBlank(message = "{user.fullName.blank}")
+    private String fullName;
+
+    @Size(min = 10, max = 10, message = "{user.phoneNum.size}")
+    @NotBlank(message = "{user.phoneNum.blank}")
+    @UniquePhoneNum
+    private String phoneNum;
+}
--- src/main/java/cybersoft/javabackend/java18/obajuecommerce/user/repository/UserGroupRepository.java
+++ src/main/java/cybersoft/javabackend/java18/obajuecommerce/user/repository/UserGroupRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -23,4 +24,6 @@ public interface UserGroupRepository extends JpaRepository<UserGroup, UUID> {
 
     @Query("select ug from UserGroup ug left join fetch ug.roles")
     List<UserGroup> findAllIncludeRole();
+
+    Optional<UserGroup> findByName(String name);
 }
