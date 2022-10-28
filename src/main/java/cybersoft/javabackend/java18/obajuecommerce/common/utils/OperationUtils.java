bc5fc85fb115253e6baa4e4360110a90aca20bd4
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-10-28 14:30:22 +0700

    feat:add-authorization-operation

---
+++ src/main/java/cybersoft/javabackend/java18/obajuecommerce/common/utils/OperationUtils.java
@@ -0,0 +1,12 @@
+package cybersoft.javabackend.java18.obajuecommerce.common.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class OperationUtils {
+    public static final String FIND_ALL = "FIND ALL";
+    public static final String FIND_BY_ID = "FIND BY ID";
+    public static final String CREATE_NEW = "CREATE NEW";
+    public static final String UPDATE = "UPDATE";
+    public static final String DELETE_BY_ID = "DELETE BY ID";
+}
